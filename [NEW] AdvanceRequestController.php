b1a1f174789be1b387c15a84e470f173f742c900
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2024-07-26 14:29:33 +0700

    Create [NEW] AdvanceRequestController.php

---
+++ [NEW] AdvanceRequestController.php
@@ -0,0 +1,64 @@
+// LIST DATA FUNCTION FOR SHOW DATA ADVANCE 
+public function AdvanceListData(Request $request)
+{
+    try {
+        $cacheKey = "DataListAdvance";
+        $sessionID = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+
+        $cachedData = \App\Helpers\ZhtHelper\Cache\Helper_Redis::getValue($sessionID, $cacheKey);
+
+        if ($cachedData === null) {
+            $varAPIWebToken = Session::get('SessionLogin');
+            $apiResponse = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                $sessionID,
+                $varAPIWebToken,
+                'transaction.read.dataList.finance.getAdvance',
+                'latest',
+                [
+                    'parameter' => null,
+                    'SQLStatement' => [
+                        'pick' => null,
+                        'sort' => null,
+                        'filter' => null,
+                        'paging' => null
+                    ]
+                ],
+                false
+            );
+
+            $cachedData = json_encode($apiResponse);
+
+            \App\Helpers\ZhtHelper\Cache\Helper_Redis::setValue(
+                $sessionID,
+                $cacheKey,
+                $cachedData
+            );
+        }
+
+        $DataListAdvance = json_decode($cachedData, true);
+
+        // Ensure DataListAdvance is an array
+        if (!is_array($DataListAdvance)) {
+            $DataListAdvance = [];
+        }
+
+        $collection = collect($DataListAdvance);
+
+        $project_id = $request->project_id;
+        $site_id = $request->site_id;
+
+        if ($project_id != "") {
+            $collection = $collection->where('CombinedBudget_RefID', $project_id);
+        }
+        if ($site_id != "") {
+            $collection = $collection->where('CombinedBudgetSection_RefID', $site_id);
+        }
+
+        $collection = $collection->values()->all();
+
+        return response()->json($collection);
+    } catch (\Throwable $th) {
+        Log::error("Error: " . $th->getMessage());
+        return response()->json(['error' => 'Process Error'], 500);
+    }
+}
