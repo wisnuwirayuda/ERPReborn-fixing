45e1a7a764155addb732a20f813241e9158172aa
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2024-08-04 23:08:20 +0700

    Update LoginController.php

--- LoginController.php
+++ LoginController.php
@@ -3,64 +3,62 @@ public function loginStore(Request $request)
     try {
         $username = $request->input('username');
         $password = $request->input('password');
-        $branchID = (int)$request->input('branch_id');
-        $roleID = (int)$request->input('role_id');
+        $varBranchID = (int)$request->input('branch_id');
+        $varUserRoleID = (int)$request->input('role_id');
 
-        if ($roleID != 0) {
-            $apiWebToken = $request->input('varAPIWebToken');
+        if ($varUserRoleID != 0) {
+
+            $varAPIWebToken = $request->input('varAPIWebToken');
             $personName = $request->input('personName');
-            $userRefID = $request->input('user_RefID');
-            $workerCareerInternalRefID = $request->input('workerCareerInternal_RefID');
+            $user_RefID = $request->input('user_RefID');
+            $workerCareerInternal_RefID = $request->input('workerCareerInternal_RefID');
             $organizationalDepartmentName = $request->input('organizationalDepartmentName');
 
-            return $this->setLoginBranchAndUserRole(
-                $apiWebToken, $branchID, $roleID, 
-                $personName, $workerCareerInternalRefID, 
-                $userRefID, $organizationalDepartmentName
+            return $this->SetLoginBranchAndUserRoleFunction(
+                $varAPIWebToken, $varBranchID, $varUserRoleID, 
+                $personName, $workerCareerInternal_RefID, 
+                $user_RefID, $organizationalDepartmentName
             );
+        } else {
+            $dataAwal = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIAuthentication(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $username,
+                $password
+            );        
+
+            if ($dataAwal['metadata']['HTTPStatusCode'] != 200) {
+                return response()->json(['status_code' => 0]);
+            }
+
+            $varAPIWebToken = $dataAwal['data']['APIWebToken'];
+            $varDataBranch = $this->GetInstitutionBranchFunction($dataAwal['data']['userIdentity']['user_RefID']);
+
+            if (count($varDataBranch) == 1) {
+                $varDataRole = $this->GetRoleFunction($varDataBranch[0]['Sys_ID'], $dataAwal['data']['userIdentity']['user_RefID']);
+
+                foreach ($varDataRole as $role) {
+                    Session::push('SessionRoleLogin', $role['Sys_ID']);
+                }
+
+                Session::put('SessionLogin', $varAPIWebToken);
+                Session::put('SessionOrganizationalDepartmentName', $dataAwal['data']['userIdentity']['organizationalDepartmentName']);
+                Session::put('SessionLoginName', $dataAwal['data']['userIdentity']['personName']);
+                Session::put('SessionWorkerCareerInternal_RefID', $dataAwal['data']['userIdentity']['workerCareerInternal_RefID']);
+                Session::put('SessionUser_RefID', $dataAwal['data']['userIdentity']['user_RefID']);
+
+                return response()->json(['status_code' => 1]);
+            } else {
+                return response()->json([
+                    'status_code' => 2,
+                    'data' => $varDataBranch,
+                    'user_RefID' => $dataAwal['data']['userIdentity']['user_RefID'],
+                    'varAPIWebToken' => $varAPIWebToken,
+                    'personName' => $dataAwal['data']['userIdentity']['personName'],
+                    'workerCareerInternal_RefID' => $dataAwal['data']['userIdentity']['workerCareerInternal_RefID'],
+                    'organizationalDepartmentName' => $dataAwal['data']['userIdentity']['organizationalDepartmentName']
+                ]);
+            }
         }
-
-        $apiResponse = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIAuthentication(
-            \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
-            $username,
-            $password
-        );
-
-        if ($apiResponse['metadata']['HTTPStatusCode'] != 200) {
-            return response()->json(['status_code' => 0]);
-        }
-
-        $apiWebToken = $apiResponse['data']['APIWebToken'];
-        $userIdentity = $apiResponse['data']['userIdentity'];
-        $userRefID = $userIdentity['user_RefID'];
-
-        $branchData = $this->getInstitutionBranch($userRefID);
-
-        if (count($branchData) == 1) {
-            $roleData = $this->getRole($branchData[0]['Sys_ID'], $userRefID);
-            $roleIDs = array_column($roleData, 'Sys_ID');
-
-            session()->put([
-                'SessionRoleLogin' => $roleIDs,
-                'SessionLogin' => $apiWebToken,
-                'SessionOrganizationalDepartmentName' => $userIdentity['organizationalDepartmentName'],
-                'SessionLoginName' => $userIdentity['personName'],
-                'SessionWorkerCareerInternal_RefID' => $userIdentity['workerCareerInternal_RefID'],
-                'SessionUser_RefID' => $userIdentity['user_RefID']
-            ]);
-
-            return response()->json(['status_code' => 1]);
-        }
-
-        return response()->json([
-            'status_code' => 2,
-            'data' => $branchData,
-            'user_RefID' => $userRefID,
-            'varAPIWebToken' => $apiWebToken,
-            'personName' => $userIdentity['personName'],
-            'workerCareerInternal_RefID' => $userIdentity['workerCareerInternal_RefID'],
-            'organizationalDepartmentName' => $userIdentity['organizationalDepartmentName']
-        ]);
     } catch (\Throwable $th) {
         return response()->json(['status_code' => 0]);
     }
