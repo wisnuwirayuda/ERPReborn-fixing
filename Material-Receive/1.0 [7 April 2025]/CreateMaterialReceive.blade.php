9765e1e1d9761928d5447e6dc2a0b24f7916ac3b
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2025-04-07 10:07:40 +0700

    Create CreateMaterialReceive.blade.php

---
+++ Material-Receive/1.0 [7 April 2025]/CreateMaterialReceive.blade.php
@@ -0,0 +1,154 @@
+@extends('Partials.app')
+@section('main')
+@include('Partials.navbar')
+@include('Partials.sidebar')
+@include('Inventory.MaterialReceive.Functions.PopUp.SearchPurchaseOrder')
+@include('Inventory.MaterialReceive.Functions.PopUp.SearchDeliveryOrder')
+@include('Inventory.MaterialReceive.Functions.PopUp.PopUpMaterialReceiveRevision')
+@include('getFunction.getWarehouse')
+
+<div class="content-wrapper">
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row mb-1" style="background-color:#4B586A;">
+                <div class="col-sm-6" style="height:30px;">
+                    <label style="font-size:15px;position:relative;top:7px;color:white;">Material Receive</label>
+                </div>
+            </div>
+            @include('Inventory.MaterialReceive.Functions.Menu.MenuMaterialReceive')
+            @if($var == 0)
+            <div class="card" style="position:relative;bottom:10px;">
+                <form method="post" enctype="multipart/form-data" action="{{ route('MaterialReceive.store') }}" id="FormSubmitMaterialReceive">
+                    @csrf
+                    <div class="tab-content p-3" id="nav-tabContent">
+                        @include('Inventory.MaterialReceive.Functions.Header.HeaderMaterialReceive')
+
+                        <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <label class="card-titl e">
+                                                File Receipt
+                                            </label>
+                                            <div class="card-tools">
+                                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                    <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(
+                                                    \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                                                    $varAPIWebToken,
+                                                    'dataInput_Log_FileUpload_1',
+                                                    null,
+                                                    'dataInput_Return'
+                                                    ).
+                                                    ''; ?>
+                                                </div>
+                                                <br><br>
+                                                <div class="col-md-12">
+                                                    <div class="card-body table-responsive p-0" style="height:125px;">
+
+                                                        <table class="table table-head-fixed table-sm text-nowrap">
+                                                            <div class="form-group input_fields_wrap">
+
+                                                                <div class="input-group control-group">
+
+                                                                    <!-- <div id="dataShow_MasterFileRecord" style="border-style:solid; border-width:1px;"></div> -->
+                                                                    <div id="dataShow_ActionPanel"></div>
+
+                                                                </div>
+                                                            </div>
+
+                                                        </table>
+
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="product-desc-tab">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <label class="card-title">
+                                                <span id="ms_type"></span> Detail
+                                            </label>
+                                            <div class="card-tools">
+                                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                    <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        @include('Inventory.MaterialReceive.Functions.Table.TableMaterialResource')
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <label class="card-title">
+                                            Material Receive Cart
+                                        </label>
+                                        <div class="card-tools">
+                                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-body table-responsive p-0 MaterialReceiveCart" style="height: 180px;">
+                                        <table class="table table-head-fixed text-nowrap table-striped TableMaterialReceiveCart" id="TableMaterialReceiveCart">
+                                            <thead>
+                                                <tr>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">Transaction Number</th>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">Product Id</th>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">Product Name</th>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">UOM</th>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">Note</th>
+                                                    <th style="padding-bottom: 10px;padding-top: 10px;border:1px solid #e9ecef;text-align: center;">Qty</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="card-body AdvanceListCart">
+                                        <table style="float:right;">
+                                            <tr>
+                                                <th style="position: relative;right:15px;"> Total Item : <span id="TotalQty">
+                                        </table>
+                                    </div>
+                                </div>
+                                <a onclick="CancelMaterialReceive();" class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                                    <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> Cancel
+                                </a>
+                                <button class="btn btn-default btn-sm float-right" type="submit" id="SubmitMaterialReceive" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                                    <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            @endif
+        </div>
+    </section>
+</div>
+@include('Partials.footer')
+@include('Inventory.MaterialReceive.Functions.Footer.FooterMaterialReceive')
+@endsection
