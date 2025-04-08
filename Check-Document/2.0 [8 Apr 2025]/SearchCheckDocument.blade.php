6590902a45dd09ab2c90548d7f3a92a869637da3
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2025-04-08 16:11:54 +0700

    Create SearchCheckDocument.blade.php

---
+++ Check-Document/2.0 [8 Apr 2025]/SearchCheckDocument.blade.php
@@ -0,0 +1,48 @@
+<div id="mySearchCheckDocument" class="modal fade" role="dialog" aria-labelledby="contohModalScrollableTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <label class="card-title">Select Document</label>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <table>
+                            <tr>
+                                <td style="padding-top: 10px;"><label>Transaction</label></td>
+                                <td>
+                                    <div class="input-group">
+                                        <select id="DocumentType" class="form-control DocumentType select2" name="DocumentType">
+                                            <option selected="selected" value=""> Select Document Type </option>
+                                        </select>
+                                    </div>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-12" style="margin-top:10px;">
+                        <div class="card">
+                            <div class="card-body table-responsive p-0" style="height: 430px;">
+                                <table class="table table-head-fixed text-nowrap TableCheckDocument" id="TableCheckDocument">
+                                    <thead>
+                                        <tr>
+                                            <th style="position:relative;left:10px;">No</th>
+                                            <th style="position:relative;left:10px;">Trano</th>
+                                            <th style="position:relative;left:10px;">Project Code</th>
+                                            <th style="position:relative;left:10px;">Site Code</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
