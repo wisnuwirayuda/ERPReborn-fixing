088fea7c4d3cdc16647558aef8934d43315f80c4
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2025-04-14 09:46:28 +0700

    Create headerPO2.blade.php

---
+++ Purchase-Order/1.0 [14 Apr 2025]/headerPO2.blade.php
@@ -0,0 +1,66 @@
+<div class="card-body">
+  <div class="row">
+    <div class="col-md-8">
+      <div class="form-group">
+        <table>
+          <tr>
+            <td style="padding-top: 12px;"><label>MSR Number</label></td>
+            <td>
+              <div class="input-group">
+                <input id="pr_number" style="border-radius:0;" name="pr_number" class="form-control" readonly>
+                <div class="input-group-append">
+                  <span style="border-radius:0;" class="input-group-text form-control">
+                    <a id="pr_number2" data-toggle="modal" data-target="#mySearchPR" class="mySearchPR"><img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt=""></a>
+                  </span>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- <div class="card-body">
+  <div class="row">
+   <div class="col-md-8">
+      <div class="form-group">
+        <table>
+          <tr>
+            <td style="padding-top: 5px;"><label>Budget Code</label></td>
+            <td>
+              <div class="input-group" style="width: 70%;">
+                <input id="projectcode" style="border-radius:0;" name="projectcode" class="form-control" readonly>
+                <div class="input-group-append">
+                  <span style="border-radius:0;" class="input-group-text form-control">
+                    <a id="projectcode2" data-toggle="modal" data-target="#myProject" class="myProject"><img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt=""></a>
+                  </span>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div class="input-group" style="width: 153%;position:relative;right:38%;">
+                <input id="projectname" style="border-radius:0;" class="form-control" name="projectname" readonly>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="form-group">
+        <table>
+          <tr>
+            <td style="padding-top: 5px;"><label>PR Number</label></td>
+            <td>
+              <label class="card-title">
+                <a href="#" id="pr_number2" data-toggle="modal" data-target="#mySearchPR"><img src="{{ asset('AdminLTE-master/dist/img/add.png') }}" width="25" alt="" style="border: 1px solid #ced4da;padding-left:4px;padding-right:4px;padding-top:2px;padding-bottom:2px;border-radius:3px;"></a>
+              </label>
+              </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</div> -->
