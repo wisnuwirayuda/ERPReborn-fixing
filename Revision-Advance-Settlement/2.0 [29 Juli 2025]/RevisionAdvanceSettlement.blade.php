4e1ebfc295ee39e027dff1e8443a84369c829e08
Wisnu Trenggono Wirayuda <71024624+wisnuwirayuda@users.noreply.github.com>
2025-07-29 14:12:20 +0700

    Update and rename CreateAdvanceSettlement.blade.php to RevisionAdvanceSettlement.blade.php

---
+++ Revision-Advance-Settlement/2.0 [29 Juli 2025]/RevisionAdvanceSettlement.blade.php
@@ -0,0 +1,272 @@
+@extends('Partials.app')
+@section('main')
+@include('Partials.navbar')
+@include('Partials.sidebar')
+@include('getFunction.getWorkFlow')
+@include('getFunction.getAdvanceSettlement')
+@include('Process.Advance.AdvanceSettlement.Functions.PopUp.PopUpAdvanceSettlementRevision')
+
+<div class="content-wrapper">
+  <section class="content">
+    <div class="container-fluid">
+      <!-- TITLE -->
+      <div class="row mb-1" style="background-color:#4B586A;">
+        <div class="col-sm-6" style="height:30px;">
+          <label style="font-size:15px;position:relative;top:7px;color:white;">
+            Advance Settlement Revision
+          </label>
+        </div>
+      </div>
+
+      @include('Process.Advance.AdvanceSettlement.Functions.Menu.MenuAdvanceSettlement')
+      <div class="card" style="position:relative;bottom:10px;">
+        <form method="POST" action="{{ route('SelectWorkFlow') }}" id="FormRevisionAdvanceSettlement">
+          @csrf
+          <input type="hidden" name="DocumentTypeID" id="DocumentTypeID">
+          <input type="hidden" name="var_combinedBudget_RefID" id="var_combinedBudget_RefID">
+
+          <!-- ADD NEW SETTLEMENT -->
+          <div class="tab-content px-3 pt-4 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Add New Settlement
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- BODY -->
+                  @include('Process.Advance.AdvanceSettlement.Functions.Header.HeaderAdvanceSettlementRevision')
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- FILE ATTACHMENT -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      File Attachment
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- BODY -->
+                  <div class="card-body">
+                    <div class="row py-3">
+                      <div class="col-lg-5">
+                        <div class="row">
+                          <div class="col p-0">
+                            <?php if (isset($fileID) && $fileID) { ?>
+                              <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1" style="display:none" value="<?= $fileID; ?>">
+                              <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                                $varAPIWebToken,
+                                'dataInput_Log_FileUpload',
+                                $fileID,
+                                'dataInput_Return'
+                                ).
+                              ''; ?>
+                            <?php } else { ?>
+                              <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1" style="display:none">
+                              <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                                $varAPIWebToken,
+                                'dataInput_Log_FileUpload',
+                                null,
+                                'dataInput_Return'
+                                ).
+                              ''; ?>
+                            <?php } ?>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- ADVANCE REQUEST DETAIL -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Advance Request Detail
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- BODY -->
+                  @include('Process.Advance.AdvanceSettlement.Functions.Table.TableAsfDetail')
+
+                  <!-- BUTTON -->
+                  <div class="card-body">
+                    <div class="row" style="gap: 1rem;">
+                      <div class="col">
+                        <table style="float:right;">
+                          <tr>
+                            <th style="position: relative;right:20px;"> Total : <span id="TotalAdvanceDetail">0.00</span></th>
+                          </tr>
+                          <tr>
+                            <td>
+                              <br>
+                              <a class="btn btn-default btn-sm float-right" id="advance-details-add" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                                <img src="{{ asset('AdminLTE-master/dist/img/add.png') }}" width="13" alt="" title="Add to Advance List"> Add
+                              </a>
+                            </td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- ADVANCE LIST (CART) -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Advance List (Cart)
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- BODY -->
+                  <div class="wrapper-budget card-body table-responsive p-0" style="height: 230px;">
+                    <table class="table table-head-fixed text-nowrap table-sm" id="tableAdvanceList">
+                      <thead style="position: sticky;top: 0px;z-index: 10;background: white;">
+                        <tr>
+                          <th rowspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">ARF Number</th>
+                          <th rowspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Product Code</th>
+                          <th rowspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Product Name</th>
+                          <th rowspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">UOM</th>
+                          <th rowspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Currency</th>
+                          <th colspan="6" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Settlement</th>
+                        </tr>
+                        <tr>
+                          <th colspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">
+                            Expense Claim
+                          </th>
+                          <th colspan="3" style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">
+                            Amount To The Company
+                          </th>
+                        </tr>
+                        <tr>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Qty</th>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Price</th>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Total</th>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Qty</th>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Price</th>
+                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center; vertical-align: middle;">Total</th>
+                        </tr>
+                      </thead>
+                      <tbody></tbody>
+                    </table>
+                  </div>
+
+                  <!-- FOOTER -->
+                  <div class="card-body">
+                    <table style="float:right;">
+                      <tr>
+                        <th> Total Item :
+                          <span id="GrandTotal">0.00</span>
+                        </th>
+                      </tr>
+                      <tr>
+                        <td>
+                          <br>
+                          <a class="btn btn-default btn-sm float-right" id="advance-details-reset" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                            <img src="{{ asset('AdminLTE-master/dist/img/reset.png') }}" width="13" alt="" title="Add to Advance List"> Reset
+                          </a>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- REMARK -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Remark
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- CONTENT -->
+                  <div class="card-body">
+                    <div class="row py-3">
+                      <textarea name="var_remark" id="remark" class="form-control"><?= $remark; ?></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- BUTTON -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col">
+                <a onclick="CancelAdvance()" class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                  <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> Cancel
+                </a>
+
+                <button class="btn btn-default btn-sm float-right" type="submit" id="submitAsf" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                  <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</div>
+
+@include('Partials.footer')
+@include('Process.Advance.AdvanceSettlement.Functions.Footer.FooterAdvanceSettlementRevision')
+@endsection
